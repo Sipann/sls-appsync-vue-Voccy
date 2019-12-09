eb66eaf8ec6032961bc0db8d15e800b3d560ec48
Sipann <vauguste92@gmail.com>
2019-12-09 18:01:59 +0100

    front end - renamed aws-amplify to aws-config

--- appsync-vue-frontend/src/main.js
+++ appsync-vue-frontend/src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
-import '@/aws-amplify'
-import { apolloProvider } from '@/aws-amplify';
+import '@/aws-config'
+import { apolloProvider } from '@/aws-config';
 
 Vue.config.productionTip = false
 
